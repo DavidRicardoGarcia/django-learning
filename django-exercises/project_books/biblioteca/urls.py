352b76fba88ec11f90dc8e1f3fde902c7bf14354
David Ricardo Garcia Sterling <davidnf.44@gmail.com>
2024-05-04 01:58:20 -0500

    Basic forms

--- django-exercises/project_books/biblioteca/urls.py
+++ django-exercises/project_books/biblioteca/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('libros/', views.lista_libros, name='lista_libros'),
+    path('libros/crear/', views.crear_libro, name='crear_libro'),
 ]
--- django-exercises/project_books/biblioteca/views.py
+++ django-exercises/project_books/biblioteca/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Libro
+from .forms import LibroForm
 
 def lista_libros(request):
     libros = Libro.objects.all()
     return render(request, 'biblioteca/lista_libros.html', {'libros': libros})
+
+def crear_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_libros')
+    else:
+        form = LibroForm()
+    
+    return render(request, 'biblioteca/crear_libro.html', {'form': form})
